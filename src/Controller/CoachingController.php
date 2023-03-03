9bc84bbd3e09c98f4a01037fb489c8a9090cc76d
CarTig <carolle.t.n@gmail.com>
2023-03-03 15:25:45 +0100

    ajout de la vue des coaching dans la home

--- src/Controller/CoachingController.php
+++ src/Controller/CoachingController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+
+use App\Repository\CoachingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class CoachingController extends AbstractController
 {
     #[Route('/coaching', name: 'app_coaching')]
-    public function index(): Response
+    public function index(CoachingRepository $repository): Response
     {
         return $this->render('coaching/index.html.twig', [
-            'controller_name' => 'CoachingController',
+            "coachings" => $repository->findAll(),
+            
         ]);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CoachingRepository;
 use App\Repository\ProgrammeRepository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,10 +12,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ProgrammeRepository $programmeRepository,): Response
+    public function index(ProgrammeRepository $programmeRepository, CoachingRepository $repository): Response
     {
         return $this->render('home/index.html.twig', [
             "programmes" => $programmeRepository->findAll(),
+            "coachings" => $repository->findAll(),
         ]);
     }
 }
